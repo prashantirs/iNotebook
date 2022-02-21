3d0183361745d969f8f7aebf10bae4ed7d11c411
Prashant Srivastava <prashantsrivastava381@gmail.com>
2022-02-21 17:34:14 +0530

    front-end stating

---
+++ src/components/About.js
@@ -0,0 +1,10 @@
+//rafce react arrow function component
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className='text-info fw-bold fs-2'> This is About</div>
+  )
+}
+
+export default About
---
+++ src/components/Home.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='text-success fw-bold fs-1 text-center'>This is Home</div>
+  )
+}
+
+export default Home
