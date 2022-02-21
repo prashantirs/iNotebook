372f5c872fe9ba9c64c98d82e88c059e7f424c14
Prashant Srivastava <prashantsrivastava381@gmail.com>
2022-02-21 17:43:58 +0530

    Created a new branch

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,7 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div className='text-info fw-bold fs-2'> This is About</div>
+    <div className='text-info fw-bold fs-2'> This is Another branch About</div>
   )
 }
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className='text-success fw-bold fs-1 text-center'>This is Home</div>
+    <div className='text-success fw-bold fs-1 text-center'>This is Another branch Home</div>
   )
 }
 
